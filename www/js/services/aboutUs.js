9ac0dc3534aaf56d6a905f67e7228c525713cc73
Shaquille <zakiakbardemsi@gmail.com>
2017-02-14 04:44:44 +0700

    dynamic api and payment api

--- www/js/services/aboutUs.js
+++ www/js/services/aboutUs.js
@@ -2,22 +2,22 @@ angular
     .module('livein')
     .service('AboutUsService', AboutUsService);
 
-    function AboutUsService($http) {
-        var service = {};
-        service.aboutus = aboutus;
-        return service;
+function AboutUsService($http, $filter) {
+    var service = {};
+    service.aboutus = aboutus;
+    return service;
 
-        function aboutus(callback) {
-            var req = {
-                method: 'GET',
-                url: 'http://innodev.vnetcloud.com/LiveIn/api/City/?action=select_datacity&idcity=1'
-            }
-            $http(req)
-                .success(function (response) {
-                    callback(response);
-                })
-                .error(function () {
-                    callback(false);
-                });
+    function aboutus(callback) {
+        var req = {
+            method: 'GET',
+            url: $filter('translate')('apilink') + 'api/City/?action=select_datacity&idcity=1'
         }
+        $http(req)
+            .success(function(response) {
+                callback(response);
+            })
+            .error(function() {
+                callback(false);
+            });
     }
+}
--- www/js/services/billing.js
+++ www/js/services/billing.js
@@ -2,69 +2,69 @@
  * Created by Lenovo on 08/12/2016.
  */
 angular
-  .module('livein')
-  .service('billingServices', billingServices);
+    .module('livein')
+    .service('billingServices', billingServices);
 
-  function billingServices($http) {
+function billingServices($http, $filter) {
     var service = {};
 
     service.loginBillingServices = loginBillingServices;
     service.getbilling = getbilling;
 
     return service;
+
     function loginBillingServices(email, callback) {
-      console.log(email);
-      var req = {
-        method: 'POST',
-        url: 'http://innodev.vnetcloud.com/liveinpayment/validatelogin',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        data: {
-          "orgID": "1",
-          "Email": email
+        console.log(email);
+        var req = {
+            method: 'POST',
+            url: $filter('translate')('apilinkpayment') + 'validatelogin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                "orgID": "1",
+                "Email": email
+            }
         }
-      }
 
-      $http(req)
-        .success(function (response) {
-          callback(response);
+        $http(req)
+            .success(function(response) {
+                callback(response);
 
-        })
-        .error(function (response) {
-          callback(response);
-        });
+            })
+            .error(function(response) {
+                callback(response);
+            });
 
 
     }
 
 
     function getbilling(email, siteId, callback) {
-      var req = {
-        method: 'POST',
-        url: 'http://innodev.vnetcloud.com/liveinpayment/getbilling',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        data: {
-          "orgID": "1",
-          "siteID": siteId,
-          "Email": email
+        var req = {
+            method: 'POST',
+            url: $filter('translate')('apilinkpayment') + 'getbilling',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                "orgID": "1",
+                "siteID": siteId,
+                "Email": email
+            }
         }
-      }
 
-      $http(req)
-        .success(function (response) {
-          console.log(response)
-          console.log(response.status)
-          callback(response);
+        $http(req)
+            .success(function(response) {
+                console.log(response)
+                console.log(response.status)
+                callback(response);
 
-        })
-        .error(function (response) {
-          console.log('false')
-          callback(false);
-        });
+            })
+            .error(function(response) {
+                console.log('false')
+                callback(false);
+            });
 
     }
-  }
-
+}
