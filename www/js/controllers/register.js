3bc7b27ad5b2a96777ff5b99903155875ceed934
Neogeekscamp <neogeekscamp@NGCMac.local>
2017-02-28 11:23:22 +0700

    register password

--- www/js/controllers/register.js
+++ www/js/controllers/register.js
@@ -2,7 +2,7 @@ angular
     .module('livein')
     .controller('register', register)
 
-    function register($scope, $ionicLoading, $ionicPopup, $location, $state, registerService, $filter) {
+    function register($scope, $ionicLoading, $ionicPopup, $location, $state, registerService, $filter,$localStorage) {
         $scope.registerManual = registerManualService;
         $scope.registerGooglePlus = registerGooglePlus;
         $scope.facebook_auth = facebook_auth;
@@ -44,259 +44,289 @@ angular
         };
 
         function registerGooglePlus() {
-            $ionicLoading.show({ template: 'Logging in...' });
-            window.plugins.googleplus.login({},
-                function(user_data) {
-                    // For the purpose of this example I will store user data on local storage
-                    registerService.setUserGoogle({
-                        userID: user_data.userId,
-                        name: user_data.displayName,
-                        email: user_data.email,
-                        picture: user_data.imageUrl,
-                        accessToken: user_data.accessToken,
-                        idToken: user_data.idToken
+
+          ionic.Platform.ready(function() {
+
+            if (window.plugins.googleplus && window.plugins){
+                 window.plugins.googleplus.isAvailable(
+                function(available) {
+
+                  window.plugins.googleplus.login(                    {
+                      'scopes':'profile email', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+                      'webClientId':'com.googleusercontent.apps.683716271520-acb1b36o862aotvbdh0d6gooolkn0ar8', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                      'offline': false, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+                    },
+                    function (obj) {
+                      console.log(JSON.stringify(obj));
+                      senddatauser(obj,"google")// do something useful instead of alerting
+                    },
+                    function (msg) {
+                      console.log('error: ' + msg);
                     });
 
-                    $ionicLoading.hide();
-                    $state.go('app.home');
-                },
-                function(msg) {
-                    $ionicLoading.hide();
-                }
-            );
-        };
+                });
+            }
+            // will execute when device is ready, or immediately if the device is already ready.
+          });
+
+        }
 
 
         // sosmed auth
 
         //with twitter
         function twitter_auth() {
-            var provider = new firebase.auth.TwitterAuthProvider();
+          ionic.Platform.ready(function () {
+            // console.log("function twitter ready");
+            //
+            //
+            // var api_key = "LuAov6upYoyXnv9MT8UegwiOX"; //Enter your Consumer Key (API Key)
+            // var api_secret = "uSpxUyRaFj5NH6I7WgUzEJMsqMRQYTeUpSRlFEgtiOIQfdc22V"; // Enter your Consumer Secret (API Secret)
+            // $cordovaOauth.twitter(api_key, api_secret).then(function(result) {
+            //   console.log(result);
+            // });
 
-            firebase.auth().signInWithPopup(provider).then(function(result) {
+            TwitterConnect.login(
+              function(result) {
+                console.log('Successful login!');
+                console.log(result);
+                getdatausertwitter();
+              }, function(error) {
+                console.log('Error logging in');
+                console.log(error);
+              }
+            );
+            console.log("udah masuk ready")
+          },function error(error) {alert(JSON.stringify(error))});
 
-
-                if (result.user.email == null) {
-                    var alertPopup = $ionicPopup.alert({
-                        title: $filter('translate')('login_failed'),
-                        template: $filter('translate')('cannot_put_email'),
-                        okText: $filter('translate')('try_again'),
-                        cssClass: "alertPopup"
-                    });
-                } else {
-                    senddatauser()
-                }
-            }).catch(function(error) {
-                // Handle Errors here.
-                var alertPopup = $ionicPopup.alert({
-                    title: $filter('translate')('login_failed'),
-                    template: error.message,
-                    okText: $filter('translate')('try_again'),
-                    cssClass: "alertPopup"
-                });
-            });
 
         }
 
-        //facebook
+      function getdatausertwitter () {
+
+        TwitterConnect.showUser(
+          function(result) {
+            console.log('User Profile:');
+            console.log(result);
+            // alert(JSON.stringify(result));
+            console.log('Twitter handle :'+result.userName);
+            if(result.email == undefined || result.email == ""){
+
+              var alertPopup = $ionicPopup.alert({
+                template: "Sorry, we can’t fetch your email for registration",
+                okText: $filter('translate')('okay'),
+                okType: "button-stable",
+                cssClass: "alertPopup"
+              });
+            }else {
+              senddatauser(result,"facebook")
+            }
+          }, function(error) {
+            console.log('Error retrieving user profile');
+            console.log(error);
+          }
+        );
+
+      }
+
+
+      //facebook
         function facebook_auth() {
-            var provider = new firebase.auth.FacebookAuthProvider();
-            provider.addScope('user_birthday');
-            provider.addScope('email')
-            provider.addScope('user_gender')
 
-
-            firebase.auth().signInWithPopup(provider).then(function(result) {
-                // This gives you a Facebook Access Token.
-                var token = result.credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                senddatauser();
-                console.log(result);
-            }).catch(function(error) {
-                var alertPopup = $ionicPopup.alert({
-                    title: $filter('translate')('login_failed'),
-                    template: error.message,
-                    okText: $filter('translate')('try_again'),
-                    cssClass: "alertPopup"
-                });
+        ionic.Platform.ready(function(){
+            facebookConnectPlugin.logout(function succes(result){
+            getloginfacebook();
+            }, function oneror(error){
+            getloginfacebook();
             })
+        });
+
+        }
+
+        function getloginfacebook () {
+
+        facebookConnectPlugin.login(["public_profile", "email", "user_birthday"], function onSucces(result) {
+
+            facebookConnectPlugin.api("me" + "/?fields=id,email,gender,birthday,name", null,
+            function onSuccess(datauser) {
+                console.log("Result: ", datauser);
+                // alert(JSON.stringify(datauser))
+                senddatauser(datauser, "facebook");
+            }, function onError(erroruser) {
+                console.error("Failed: ", erroruser);
+            }
+            );
+
+        }, function onError(error) {
+            var alertPopup = $ionicPopup.alert({
+            title: 'Facebook Login Failed ..',
+            template: $filter('translate')('user_cancelled'),
+            okText: $filter('translate')('okay'),
+            okType: "button-stable",
+            cssClass: "alertPopup"
+            });
+        });
+
         }
 
 
         //google auth
         function google_auth() {
 
-            var provider = new firebase.auth.GoogleAuthProvider();
-            provider.addScope('profile');
-            provider.addScope('email');
-            provider.addScope('https://www.googleapis.com/auth/plus.login');
+          ionic.Platform.ready(function() {
 
-            firebase.auth().signInWithPopup(provider).then(function(result) {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                result_user = result.user;
-                console.log(result_user);
+            if (window.plugins.googleplus && window.plugins){
+              window.plugins.googleplus.isAvailable(
+                function(available) {
 
-                //send data to api
-                senddatauser();
+                  window.plugins.googleplus.login(                    {
+                      'scopes':'profile email', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+                      'webClientId':'com.googleusercontent.apps.683716271520-acb1b36o862aotvbdh0d6gooolkn0ar8', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                      'offline': false, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+                    },
+                    function (obj) {
+                      console.log(JSON.stringify(obj));
+                      senddatauser(obj,"google")// do something useful instead of alerting
+                    },
+                    function (msg) {
+                      console.log('error: ' + msg);
+                    });
 
-            }).catch(function(error) {
-                // Handle Errors here
-                // .
-                var alertPopup = $ionicPopup.alert({
-                    title: $filter('translate')('login_failed'),
-                    template: error.message,
-                    okText: $filter('translate')('try_again'),
-                    cssClass: "alertPopup"
                 });
-                // ...
-            });
+            }
+            // will execute when device is ready, or immediately if the device is already ready.
+          });
 
 
         }
 
-        function senddatauser() {
-            registerService.registerManualService(
-                result_user.displayName,
-                'unknow',
-                Math.random(),
-                result_user.email,
-                Math.random(),
-                function(response) {
-                    if (response != false) {
-                        console.log(response);
-                        var alertPopup = $ionicPopup.alert({
-                            title: $filter('translate')('login_success'),
-                            template: $filter('translate')('dialog_success_upgrade'),
-                            okText: $filter('translate')('yes'),
-                            cssClass: "alertPopup"
-                        });
-                        $state.go('app.main');
-                    } else {
-                        var alertPopup = $ionicPopup.alert({
-                            title: $filter('translate')('registration_failed'),
-                            template: $filter('translate')('email_exist'),
-                            okText: $filter('translate')('yes'),
-                            cssClass: "alertPopup"
-                        }); //tetap di halaman register//muncul alert phone or email alredy exist->dari api persis
-                        $state.go('/login');
-                    }
-                    $ionicLoading.hide();
-                });
-        }
+      //action of login and register
+        function senddatauser(data_user, sosmed) {
 
-      function senddatauser(data_user, sosmed) {
-
-        $ionicLoading.show({ template: 'Logging In ...', duration: 2000 });
+        $ionicLoading.show({ template: $filter('translate')('loginmessage') + "...", duration: 2000 });
 
         if(sosmed == "facebook") {
 
-          registerService.registerManualService(
+          // alert("send data from facebook")
+
+            registerService.registerManualService(
             data_user.name,
             data_user.gender,
             Math.random(),
             data_user.email,
             Math.random(),
             function (response) {
-              alert(JSON.stringify(response))
-              alert(Math.random());
-              if (response != false) {
+                if (response != false) {
                 if (response[0].status == false) {
+                  // alert(JSON.stringify(response))
 
-                  //$localStorage.currentUser = { data : response[0] };
-                  $state.go('app.main');
+                    $localStorage.currentUser = { data : response };
+                    $state.go('app.main');
 
-                  var alertPopup = $ionicPopup.alert({
+                    var alertPopup = $ionicPopup.alert({
                     template: $filter('translate')('hello') + ' ' + response[0].fullname + '. ' + $filter('translate')('welcome_dialog') + ' <strong>' + response[0].privilege + '!</strong> ',
                     okText: $filter('translate')('okay'),
                     okType: "button-stable",
                     cssClass: "alertPopup"
+                    });
 
-                  });
-                  console.log("false")
+                    alertPopup.then(function (res) {
+                    if (res) {
+                        AdvertiseService.AdsLogin();
+                    }
+                    });
+
+                    console.log("false")
 
                 } else if (response[0].status == true) {
-                  var alertPopup = $ionicPopup.alert({
+
+                  alert(JSON.stringify(response));
+                    var alertPopup = $ionicPopup.alert({
                     title: $filter('translate')('registration_success'),
                     template: $filter('translate')('check_email'),
                     okText: $filter('translate')('okay'),
                     okType: "button-stable",
                     cssClass: "alertPopup"
-                  });
-                  console.log("true")
+                    });
+                    console.log("true")
 
                 }
 
                 console.log("true tapi false api yo gak true")
 
-              } else {
+                } else {
                 var alertPopup = $ionicPopup.alert({
-                  title: $filter('translate')('registration_failed'),
-                  template: $filter('translate')('email_exist'),
-                  okText: $filter('translate')('okay'),
-                  okType: "button-stable",
-                  cssClass: "alertPopup"
+                    title: $filter('translate')('registration_failed'),
+                    template: $filter('translate')('email_exist'),
+                    okText: $filter('translate')('okay'),
+                    okType: "button-stable",
+                    cssClass: "alertPopup"
                 });
 
                 //tetap di halaman register//muncul alert phone or email alredy exist->dari api persis
                 $state.go('/login');
-              }
-              $ionicLoading.hide();
+                }
+                $ionicLoading.hide();
             });
         } else if (sosmed == "google"){
-          registerService.registerManualService(
+
+            registerService.registerManualService(
             data_user.displayName,
             "n",
             Math.random(),
             data_user.email,
             Math.random(),
             function (response) {
-              if (response != false) {
+                if (response != false) {
                 if (response[0].status == false) {
 
-                  $state.go('app.main');
+                    $state.go('app.main');
+                    $localStorage.currentUser = { data : response };
 
-
-                  $localStorage.currentUser = { data : response[0] };
-
-                  var alertPopup = $ionicPopup.alert({
+                    var alertPopup = $ionicPopup.alert({
                     template: $filter('translate')('hello') + ' ' + response[0].fullname + '. ' + $filter('translate')('welcome_dialog') + ' <strong>' + response[0].privilege + '!</strong> ',
                     okText: $filter('translate')('okay'),
                     okType: "button-stable",
                     cssClass: "alertPopup"
+                    });
 
-                  });
-                  console.log("false")
+                    alertPopup.then(function (res) {
+                    if (res) {
+                        AdvertiseService.AdsLogin();
+                    }
+                    });
+
+                    console.log("false")
 
                 } else if (response[0].status == true) {
-                  var alertPopup = $ionicPopup.alert({
+                    var alertPopup = $ionicPopup.alert({
                     title: $filter('translate')('registration_success'),
                     template: $filter('translate')('check_email'),
                     okText: $filter('translate')('okay'),
                     okType: "button-stable",
                     cssClass: "alertPopup"
-                  });
-                  console.log("true")
+                    });
+                    console.log("true")
 
                 }
 
-              } else {
+                } else {
                 var alertPopup = $ionicPopup.alert({
-                  title: $filter('translate')('registration_failed'),
-                  template: $filter('translate')('email_exist'),
-                  okText: $filter('translate')('okay'),
-                  okType: "button-stable",
-                  cssClass: "alertPopup"
+                    title: $filter('translate')('registration_failed'),
+                    template: $filter('translate')('email_exist'),
+                    okText: $filter('translate')('okay'),
+                    okType: "button-stable",
+                    cssClass: "alertPopup"
                 });
 
                 //tetap di halaman register//muncul alert phone or email alredy exist->dari api persis
                 $state.go('/login');
-              }
-              $ionicLoading.hide();
+                }
+                $ionicLoading.hide();
             });
 
+          }
         }
-      }
 
 
 
