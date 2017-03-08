e99d47b7b06e9b5c77cc3855a54359e403367a33
Faisal Alfareza <faisal.alfareza@visionet.co.id>
2017-03-08 14:14:27 +0700

    Removing Train Menu

--- www/js/app.js
+++ www/js/app.js
@@ -1635,9 +1635,9 @@ function run($ionicPlatform, $ionicPopup, $timeout, $rootScope, $location, $filt
 
     $ionicPlatform.ready(function() {
 
-        cordova.plugins.notification.local.clearAll(function(){
-            console.log("LocalNotificationPlugin: ClearAll callback a success.");
-        });
+        // cordova.plugins.notification.local.clearAll(function(){
+        //     console.log("LocalNotificationPlugin: ClearAll callback a success.");
+        // });
 
         // cordova.plugins.notification.local.clearAll(function() {
         //     alert("clearAll Done!");
