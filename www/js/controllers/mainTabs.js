307c8015da90cd57e64ca67331365bf8f2251351
Neogeekscamp <neogeekscamp@NGCMac.local>
2017-04-26 12:02:56 +0700

    Flow Rate Tenant

--- www/js/controllers/mainTabs.js
+++ www/js/controllers/mainTabs.js
@@ -4,6 +4,69 @@ angular
 
     function mainTabs($scope, $rootScope, $timeout, $window, $ionicPopup, $localStorage, $ionicLoading, $ionicModal, $ionicSlideBoxDelegate, dataWhatsNew, talktoUs, $filter) {
 
+        // function startIntroduction() {
+
+        //     $scope.CompletedEvent = function () {
+        //         console.log("Completed Event called");
+        //     };
+
+        //     $scope.ExitEvent = function () {
+        //         console.log("Exit Event called");
+        //     };
+
+        //     $scope.ChangeEvent = function (targetElement) {
+        //         console.log("Change Event called");
+        //         console.log(targetElement);
+        //     };
+
+        //     $scope.BeforeChangeEvent = function (targetElement) {
+        //         console.log("Before Change Event called");
+        //         console.log(targetElement);
+        //     };
+
+        //     $scope.AfterChangeEvent = function (targetElement) {
+        //         console.log("After Change Event called");
+        //         console.log(targetElement);
+        //     };
+
+        //     $scope.IntroOptions = {
+        //         steps:[
+        //         {
+        //             element: document.querySelector('#step1'),
+        //             intro: "This is the first tooltip."
+        //         },
+        //         {
+        //             element: document.querySelectorAll('#step2')[0],
+        //             intro: "<strong>You</strong> can also <em>include</em> HTML",
+        //             position: 'right'
+        //         },
+        //         {
+        //             element: '#step3',
+        //             intro: 'More features, more fun.',
+        //             position: 'left'
+        //         },
+        //         {
+        //             element: '#step4',
+        //             intro: "Another step.",
+        //             position: 'bottom'
+        //         },
+        //         {
+        //             element: '#step5',
+        //             intro: 'Get it, use it.'
+        //         }
+        //         ],
+        //         showStepNumbers: false,
+        //         showBullets: false,
+        //         exitOnOverlayClick: true,
+        //         exitOnEsc:true,
+        //         nextLabel: '<strong>NEXT!</strong>',
+        //         prevLabel: '<span style="color:green">Previous</span>',
+        //         skipLabel: 'Exit',
+        //         doneLabel: 'Thanks'
+        //     };
+
+        // }
+
         $scope.afliates_ovo = isOVO;
 
         function isOVO() {
