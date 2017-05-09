7614e8b67f0df097fdadc73a3df137cdf3ee04b0
Neogeekscamp <neogeekscamp@NGCMac.local>
2017-05-09 16:35:36 +0700

    Update Asem

--- www/js/controllers/mainTabs.js
+++ www/js/controllers/mainTabs.js
@@ -5,6 +5,7 @@ angular
     function mainTabs($scope, $rootScope, $timeout, $window, $ionicPopup, $localStorage, $ionicLoading, $ionicModal, $ionicSlideBoxDelegate, $ionicPlatform, dataWhatsNew, talktoUs, $filter, AdvertiseService) {
 
         $rootScope.StartEvent = false;
+        console.log($rootScope.StartEvent);
         
         if ($localStorage.firstOpen == null) {
             startIntroduction();
